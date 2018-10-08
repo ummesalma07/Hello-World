180e7e6ff1354d5d92853d96ad08ba6203e2851d
ummesalma07 <43916050+ummesalma07@users.noreply.github.com>
2018-10-08 18:46:11 -0400

    Create palindrome.rb

---
+++ palindrome.rb
@@ -0,0 +1,18 @@
+def reverse_string(string)
+  split_string = string.split("")
+  reversed = []
+  string.size.times { reversed << split_string.pop }
+  #puts reversed.to_s
+  puts reversed.join
+  end
+
+def palindrome?(string)
+  if string[0] == string[-1]
+      palindrome?(string[1..-1])
+    return true
+    else
+      return false
+    end
+  end
+puts reverse_string("sos")
+puts palindrome?("sos")
